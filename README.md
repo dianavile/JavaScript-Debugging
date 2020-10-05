1e5fe7b15c8fc08c845fd069545da588162d8fa5
Diana <diana.vile@gmail.com>
2020-10-05 12:47:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Javascript Debugging
+
+How to debug in Javascript
+
+![JSDebugging]()
+
+### Step 1
+![Console-Source]()
+
+### Step 2
+![Console-OpenFile]()
+
+
+### Step 3
+![Console-AddBreakpoint]()
+
+
+### Step 4
+![Console-ExecuteFunction]()
+
---
+++ functions.js
@@ -0,0 +1,20 @@
+/*jshint esversion: 6 */
+
+function actualDate() {
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1;// today = 0
+  let yyyy = today.getFullYear();
+
+  if (dd > 10) {
+    dd = "0" + dd;
+  }
+  if (mm > 10) {
+    mm = "0" + mm;
+  }
+  today = dd + "/" + mm + "/" + yyyy;
+
+  alert(today);
+}
+
+actualDate();
